bd92acea67256110631b2741678de1469ccbe051
Sayali Alatkar <sayali7.alatkar@gmail.com>
2021-10-24 18:16:30 -0500

    Create init_tcpdump.sh

---
+++ init_tcpdump.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sleep 30
+
+while true
+do 
+        sudo timeout 5s tcpdump -i eth0 -s96 'ip or icmp or tcp or udp' -w $HOME/iot_rule_client/tcpdumpout/tcpdumpout.`date "+%Y_%m_%d_%T"`.pcap 2>&1
+done
+
