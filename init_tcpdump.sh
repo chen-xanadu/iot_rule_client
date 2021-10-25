3bff8f3bb04987cb2491b7da9de0dc8d801c37f6
Sayali Alatkar <sayali7.alatkar@gmail.com>
2021-10-25 09:35:19 -0500

    Update init_tcpdump.sh

--- init_tcpdump.sh
+++ init_tcpdump.sh
@@ -4,6 +4,6 @@ sleep 30
 
 while true
 do 
-        sudo timeout 5s tcpdump -i eth0 -s96 'ip or icmp or tcp or udp' -w $HOME/iot_rule_client/tcpdumpout/tcpdumpout.`date "+%Y_%m_%d_%T"`.pcap 2>&1
+        sudo timeout 30m tcpdump -i eth0 -s96 'ip or icmp or tcp or udp' -w $HOME/iot_rule_client/tcpdumpout/tcpdumpout.`date "+%Y_%m_%d_%T"`.pcap 2>&1
 done
 
