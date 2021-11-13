a05d0183b9e49d455185dec563e79ad0bf44dfef
Sayali Alatkar <sayali7.alatkar@gmail.com>
2021-11-13 09:44:51 -0600

    Update init_tcpdump.sh

--- init_tcpdump.sh
+++ init_tcpdump.sh
@@ -16,6 +16,9 @@ while true
 do 
         curr_date=$(date "+%Y_%m_%d_%T")
         sudo timeout 30m tcpdump -i eth0 -s96 'ip or icmp or tcp or udp' -w $HOME/iot_rule_client/tcpdumpout/tcpdumpout.${curr_date}.pcap 2>&1
+        
+        # UNCOMMENT LINE TO COLLECT ENTIRE PACKETS
+        #sudo timeout 30m tcpdump -i eth0 -s96 'ip or icmp or tcp or udp' -w $HOME/iot_rule_client/tcpdumpout/tcpdumpout.${curr_date}.pcap 2>&1
         ls -t $HOME/iot_rule_client/tcpdumpout/tcpdumpout.* | head -n 1 | xargs -I XX tshark -r XX -T json > $HOME/iot_rule_client/tcpdumpoutJSON/tcpdumpoutJSON.${curr_date}.json
 done
 
